c97ee411a5462dd2ba3af71c5fd1b0d1c5980bd5
Michael Eden <michael.eden100@gmail.com>
2022-04-05 00:16:09 -0400

    Refactor sequentialClassifier.m

--- Lab2/sequentialClassifier.m
+++ Lab2/sequentialClassifier.m
@@ -1,73 +1,67 @@
-function [discriminants, true_n_ab, true_n_ba] = sequentialClassifier(A,B,J)
-    true_n_ab = {};
-    true_n_ba = {};
-    j = 1;
-    discriminants = {};
+function [G, n_ab, n_ba] = sequentialClassifier(A,B,J)
+    n_ab = {};
+    n_ba = {};
+    G = {};
 
+    j = 1;
     while true
         while true
-            b_as_a_error = 0;
-            a_as_b_error = 0;
-            temp_a = [];
-            temp_b = [];
-            classA = zeros(size(A,1),1);
-            classB = zeros(size(B,1),1);
+            b_as_a_error = [];
+            a_as_b_error = [];
 
+            % Select one random point each from A and B.
             if (size(A,1) > 0)
-                a_rand = ceil(rand * size(A,1)); % select the first element of the random row
+                a_rand = ceil(rand * size(A,1));
                 a_mu = A(a_rand,:);
             end
             if (size(B,1) > 0)
-                b_rand = ceil(rand * size(B,1)); % select the first element of the random row
+                b_rand = ceil(rand * size(B,1));
                 b_mu = B(b_rand,:);
             end
 
             % Classify all points (For class A)
             for i=1:size(A,1)
                 x = A(i,:);
-                classA(i) = calculateMED(x(1),x(2),a_mu,b_mu);
+                x_class = calculateMED(x(1),x(2),a_mu,b_mu);
 
-                if (classA(i) == 2) % if classified as class B
-                    % Add to error count
-                    a_as_b_error = a_as_b_error + 1;
+                if (x_class == 2) % if classified as class B
                     % Add misclassified point
-                    temp_a(end+1,:) = x;
+                    a_as_b_error(end+1,:) = x;
                 end
             end
 
-            % Class B Classifier
+            % Classify all points (for class B)
             for i=1:size(B,1)
                 x = B(i,:);
-                classB(i) = calculateMED(x(1),x(2),a_mu,b_mu);
+                x_class = calculateMED(x(1),x(2),a_mu,b_mu);
 
-                if (classB(i) == 1)
-                   b_as_a_error = b_as_a_error + 1;
-                   temp_b(end+1,:) = x;
+                if (x_class == 1)
+                   b_as_a_error(end+1,:) = x;
                 end
             end
 
+            n_ab_j = size(a_as_b_error,1);
+            n_ba_j = size(b_as_a_error,1);
+
             % exit loop if there is no misclassified
-            if (a_as_b_error == 0 || b_as_a_error == 0)
-               if (a_as_b_error == 0)
-                   B = temp_b;
+            if (n_ab_j == 0 || n_ba_j == 0)
+               if (n_ab_j == 0)
+                   B = b_as_a_error;
                end
-               if (b_as_a_error == 0)
-                   A = temp_a;
+               if (n_ba_j == 0)
+                   A = a_as_b_error;
                end
                break;
             end
         end
 
         % Remember the points for that particular discriminant
-        discriminants = [discriminants, [a_mu; b_mu]];
-        true_n_ab = [true_n_ab, a_as_b_error];
-        true_n_ba = [true_n_ba, b_as_a_error];
+        G = [G, [a_mu; b_mu]];
+        n_ab = [n_ab, n_ab_j];
+        n_ba = [n_ba, n_ba_j];
 
         if (size(A,1) == 0 && size(B,1) == 0)
             break;
-        end
-        if (J ~= 0)
-            % do nothing
         end
         if (J ~= 0 && j >= J)
             break;
